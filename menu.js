f6e9233b8b506248ae8cede2a36583f57c7b3418
autumnlydon <autumnlydon@gmail.com>
2024-08-14 14:58:45 -0400

    first commit

---
+++ menu.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const mainNav = document.getElementById('main-nav');
+
+    menuToggle.addEventListener('click', function() {
+        mainNav.classList.toggle('active');
+    });
+});
